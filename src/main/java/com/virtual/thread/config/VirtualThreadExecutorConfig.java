02b1b210fa1b96964604e43127f2961247bd58ba
Anil tripathi <aniltripathi@Anils-MacBook-Pro-2.local>
2025-08-16 13:49:57 +0530

    virtual thread config updates

--- src/main/java/com/virtual/thread/config/VirtualThreadExecutorConfig.java
+++ src/main/java/com/virtual/thread/config/VirtualThreadExecutorConfig.java
@@ -3,14 +3,15 @@ package com.virtual.thread.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
 public class VirtualThreadExecutorConfig {
 
-    @Bean(name = "virtualThreadExecutor")
-    public Executor virtualThreadExecutor() {
+    // For internal async/background operations
+    @Bean(name = "internalAsyncExecutor")
+    public ExecutorService internalAsyncExecutor() {
         return Executors.newVirtualThreadPerTaskExecutor();
     }
 }
